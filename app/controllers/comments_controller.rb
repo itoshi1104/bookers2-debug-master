811693a171b754907f0bfde403e5f7a14fa100a5
itoshi1104 <ssj3itoshi1104@gmail.com>
2021-04-09 12:27:44 +0000

    comments contoller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+    before_action :correct_user, only: [:destroy]
+
+  def create
+    book = Book.find(params[:article_id])
+    comment = Comment.new(comment_params)
+    comment.article_id = article.id
+    comment.user_id = current_user.id
+    @book = comment.book
+    if comment.save
+    @book.create_notification_comment!(current_user, comment.id)
+      redirect_back(fallback_location: root_path) 
+    else
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:article_id])
+    comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def correct_user
+    comment = Comment.find(params[:article_id])
+    redirect_to book_path if current_user.id != comment.user_id
+  end
 end
